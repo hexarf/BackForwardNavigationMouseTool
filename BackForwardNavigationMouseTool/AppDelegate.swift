29e688790089018a1879ebc36c0ab1463ab8e25c
Byungjun Park <hexarf@gmail.com>
2025-04-18 22:50:56 +0900

    Initial Commit

---
+++ BackForwardNavigationMouseTool/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  BackForwardNavigationMouseTool
+//
+//  Created by PARK BYUNGJUN on 2025/04/18.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
